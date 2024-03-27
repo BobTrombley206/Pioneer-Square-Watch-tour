524e7fc52baa0a538560761e104c59cc2806f95b
robert trombley <cloudyandcoolprod@gmail.com>
2024-03-27 05:43:06 -0700

    Initial Commit

---
+++ Pioneer Square Watch tour/Pioneer_Square_Watch_tourApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pioneer_Square_Watch_tourApp.swift
+//  Pioneer Square Watch tour
+//
+//  Created by Wilfred Trombley on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Pioneer_Square_Watch_tourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pioneer Square Watch tourUITests/Pioneer_Square_Watch_tourUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pioneer_Square_Watch_tourUITestsLaunchTests.swift
+//  Pioneer Square Watch tourUITests
+//
+//  Created by Wilfred Trombley on 3/27/24.
+//
+
+import XCTest
+
+final class Pioneer_Square_Watch_tourUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
